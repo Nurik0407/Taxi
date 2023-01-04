272a5f8415437fdce6c19cb55643e590ced1f26d
99670 <bakaskoichubaev312@gmail.com>
2023-01-04 18:50:09 +0600

    create pacege service

---
+++ src/service/ClientService.java
@@ -0,0 +1,22 @@
+package service;
+
+import classes.Client;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ClientService {
+    String addClient(Client client);
+
+    String addClient(List<Client> clients);
+
+    List<Client> getClientByName(String name);
+
+    Client removeClientById(Long id);
+
+    Taxi orderTaxi(Long clientId, String taxiType);
+
+    Map<Integer, Client> getClientAge();
+
+    void universalSorting(String word);
+}
