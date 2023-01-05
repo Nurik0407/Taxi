5b6a5572abf4ef998bdb304711ecf1832ac030b2
Nurik <nuradil936@gmail.com>
2023-01-05 18:35:40 +0600

    added command main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
+import classes.Client;
 import classes.Taxi;
 import dao.DateBase;
 import enums.TaxiType;
+import service.impl.ClientServiceImpl;
 import service.impl.TaxiServiceImpl;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     static ClientServiceImpl client = new ClientServiceImpl();
     static TaxiServiceImpl taxi = new TaxiServiceImpl();
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
 
 /**  * Жаны проект ачып ичине томондогу класстарды тузунуз:
  * 1. Client (Long id, String fullName, LocalDate dateOfBirth, String phoneNumber, BigDecimal money);
@@ -63,21 +64,17 @@ public class Main {
  *
  *  P.S. Жогорудагы берилген методдорду озгортууго болбойт, бирок кошумча метод кошсонуздар болот. **/
 
-//        TaxiServiceImpl taxiService = new TaxiServiceImpl();
-//
-//        List<Taxi> taxis = new ArrayList<>(Arrays.asList(
-//                new Taxi(1L,"BMV","08KG456AGC","RED", LocalDate.of(2019,5,6), TaxiType.COMFORT),
-//                new Taxi(2L,"LEXUS","08KG456AGC","BLACK", LocalDate.of(2016,7,6), TaxiType.BUSINESS),
-//                new Taxi(3L,"AUDI","08KG456AGC","WHITE", LocalDate.of(2006,6,6), TaxiType.STANDART),
-//                new Taxi(4L,"KIA","08KG456AGC","YELLOW", LocalDate.of(2013,4,6), TaxiType.BUSINESS),
-//                new Taxi(5L,"TOYOTA","08KG456AGC","BLUE", LocalDate.of(2014,2,6), TaxiType.COMFORT)
-//        ));
-//
-//
-//        System.out.println(taxiService.add(taxis));
-//        System.out.println(taxiService.add(new Taxi(6L, "AUDI", "08KG765DFG", "RED", LocalDate.of(2016, 5, 4), TaxiType.COMFORT)));
-//
-//        System.out.println(taxiService.grouping());
+        List<Taxi> taxis = new ArrayList<>(Arrays.asList(
+                new Taxi(1L, "BMV", "08KG456AGC", "RED", LocalDate.of(2019, 5, 6), TaxiType.COMFORT),
+                new Taxi(2L, "LEXUS", "08KG456AGC", "BLACK", LocalDate.of(2016, 7, 6), TaxiType.BUSINESS),
+                new Taxi(3L, "AUDI", "08KG456AGC", "WHITE", LocalDate.of(2006, 6, 6), TaxiType.STANDART),
+                new Taxi(4L, "KIA", "08KG456AGC", "YELLOW", LocalDate.of(2013, 4, 6), TaxiType.BUSINESS),
+                new Taxi(5L, "TOYOTA", "08KG456AGC", "BLUE", LocalDate.of(2014, 2, 6), TaxiType.COMFORT)
+        ));
+        Taxi taxi1 = new Taxi(6L, "AUDI", "08KG765DFG", "RED", LocalDate.of(2016, 5, 4), TaxiType.COMFORT);
+
+
+//         System.out.println(taxiService.grouping());
 //        System.out.println("Write taxi ID: ");
 //        long id=new Scanner(System.in).nextLong();
 //        taxiService.update(id);
@@ -90,12 +87,11 @@ public class Main {
 //        System.out.println(taxiService.filterByTaxiType(type));
 
 
-        Client client1 = new Client(1L,"Saskaraeva Meerim",LocalDate.of(1995,2,5),"0500322332",BigDecimal.valueOf(500));
+        Client client1 = new Client(1L, "Saskaraeva Meerim", LocalDate.of(1995, 2, 5), "0500322332", BigDecimal.valueOf(500));
 
         List<Client> clients = new ArrayList<>(Arrays.asList(
-                new Client(2L)
+                new Client(2L, "Koichubaev Bakas", LocalDate.of(1995, 4, 6), "0708627482", BigDecimal.valueOf(700))
         ));
-
 
 
         while (true) {
@@ -117,14 +113,77 @@ public class Main {
                             7-universal sorting      
                                   """);
                     int t = new Scanner(System.in).nextInt();
-                    switch (t){
-                        case 1-> System.out.println(client.addClient(client1));
-                        case 2->
+                    switch (t) {
+                        case 1 -> System.out.println(client.addClient(client1));
+                        case 2 -> System.out.println(client.addClient(clients));
+                        case 3 -> {
+                            System.out.println("Enter client name: ");
+                            String name = new Scanner(System.in).nextLine();
+                            client.getClientByName(name);
+                        }
+                        case 4 -> {
+                            System.out.println("Enter client ID: ");
+                            Long id = new Scanner(System.in).nextLong();
+                            System.out.println(client.removeClientById(id));
+                        }
+                        case 5 -> {
+                            System.out.println("Enter client ID: ");
+                            Long clientId = new Scanner(System.in).nextLong();
+                            System.out.println("Enter taxi type: ");
+                            String taxiType = new Scanner(System.in).nextLine();
+                            System.out.println(client.orderTaxi(clientId, taxiType));
+                        }
+                        case 6 -> System.out.println(client.getClientAge());
+                        case 7 -> {
+                            System.out.println("How will you sort: ");
+                            System.out.println("""
+                                    Id
+                                    fullName
+                                    DateOfBirth
+                                    PhoneNumber
+                                    Money
+                                    """);
+                            String word = new Scanner(System.in).nextLine();
+                            client.universalSorting(word);
+                        }
+                    }
+                }
+                case 2 -> {
+                    System.out.println("""
+                            1-added  taxi
+                            2-added taxis
+                            3-find by initial  letter
+                            4-grouping
+                            5-filter by taxi type
+                            6-ubdate
+                            """);
+                    int l = new Scanner(System.in).nextInt();
+                    switch (l) {
+                        case 1 -> System.out.println(taxi.add(taxi1));
+                        case 2 -> System.out.println(taxi.add(taxis));
+                        case 3 -> {
+                            System.out.println("Enter letter: ");
+                            String model = new Scanner(System.in).nextLine();
+                            System.out.println(taxi.findByInitialLetter(model));
+                        }
+                        case 4 -> System.out.println(taxi.grouping());
+                        case 5 -> {
+                            System.out.println("Enter type taxi: ");
+                            String typeTaxi = new Scanner(System.in).nextLine();
+                            System.out.println(taxi.filterByTaxiType(typeTaxi));
+                        }
+                        case 6 -> {
+                            System.out.println("Enter taxi ID: ");
+                            Long id = new Scanner(System.in).nextLong();
+                            taxi.update(id);
+                        }
                     }
                 }
 
-
             }
+
+
+        }
 
     }
 
